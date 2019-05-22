5f93eec313c4bb1bdb3449d73ad1bc1bb370dcde
Claire McCarthy <clmccart@microsoft.com>
2019-05-22 15:16:12 -0700

    Clean up train.py

--- train.py
+++ train.py
@@ -10,13 +10,13 @@ from azureml.core.run import Run
 
 run = Run.get_context()
 
-clf = svm.SVC(gamma='scale')
 iris = datasets.load_iris()
 X, y = iris['data'], iris['target']
 
 # for simplicity's sake, just using the test set as the val set. not best practice
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
+clf = svm.SVC(gamma='scale')
 clf.fit(X_train, y_train)  
 
 train_acc = cross_val_score(clf, X_train, y_train, scoring='accuracy', cv=2)  
